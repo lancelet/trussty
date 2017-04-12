b2ea07ded56806e83028effa84b53c08022e70e0
Jonathan Merritt <j.s.merritt@gmail.com>
2017-04-12 20:07:21 +1000

    Prepare for extensible effects

--- src/Trussty/GaussJordan.hs
+++ src/Trussty/GaussJordan.hs
@@ -1,2 +1,23 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE UndecidableInstances  #-}
+
 module Trussty.GaussJordan where
+
+import qualified Data.Vector                  as V
+import qualified Data.Vector.Unboxed          as U
+import           Data.Vector.Unboxed.Deriving (derivingUnbox)
+import           Data.Word                    (Word16)
+
+-------------------------------------------------------------------------------
+-- * Basic types
+
+type Index = Word16
+    
+newtype RowIndex = RowIndex { unRowIndex :: Index } deriving (Show, Eq, Ord)
+
+newtype ColIndex = ColIndex { unColIndex :: Index } deriving (Show, Eq, Ord)
 
